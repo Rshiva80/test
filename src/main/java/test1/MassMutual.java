b244bc71be4b78facd55005e5e4c8fae5b342f39
ruby.shiva <ruby.shiva@10.1.161.250>
2022-06-27 18:17:33 -0400

    FIRST Commit

---
+++ src/main/java/test1/MassMutual.java
@@ -0,0 +1,17 @@
+package test1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MassMutual {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver" , "/Users/ruby.shiva/Desktop/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://worksite.genuinedev.com/");
+		driver.close();
+
+	}
+
+}
