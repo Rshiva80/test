17f1d8dd1652d2761696f91de70682f6c07e2194
ruby.shiva <ruby.shiva@10.1.161.250>
2022-06-27 21:30:56 -0400

    Added add method.

--- src/main/java/test1/MassMutual.java
+++ src/main/java/test1/MassMutual.java
@@ -4,13 +4,19 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MassMutual {
+	
+	void add(int x, int y) {
+		System.out.println(x+y);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver" , "/Users/ruby.shiva/Desktop/chromedriver");
-		WebDriver driver = new ChromeDriver();
-		driver.get("http://worksite.genuinedev.com/");
-		System.out.println("Worksite opened");
+//		System.setProperty("webdriver.chrome.driver" , "/Users/ruby.shiva/Desktop/chromedriver");
+//		WebDriver driver = new ChromeDriver();
+//		driver.get("http://worksite.genuinedev.com/");
+//		System.out.println("Worksite opened");
+		
+		new MassMutual().add(10,13);
 	}
 
 }
